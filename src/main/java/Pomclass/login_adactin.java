ad586d89493267efa03d0e8fc0fc803f6a06c54c
brockmohan1319@gmail.com <brockmohan1319@gmail.com>
2024-01-31 11:45:19 +0530

    POM CLASS

---
+++ src/main/java/Pomclass/login_adactin.java
@@ -0,0 +1,11 @@
+package Pomclass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class login_adactin {
+	@tes
+	public void LoginAdactinI() {
+		
+	}
+}
---
+++ src/main/java/com/Pom_Object_MODEL/App.java
@@ -0,0 +1,13 @@
+package com.Pom_Object_MODEL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
